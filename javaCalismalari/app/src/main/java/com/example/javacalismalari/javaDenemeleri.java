04e7b6eeeaef76d53488af1abda4ddd80a902fc1
Kade Kod <kefgir34@gmail.com>
2022-05-05 21:50:03 +0300

    Java Çalışmaları

---
+++ javaCalismalari/app/src/main/java/com/example/javacalismalari/javaDenemeleri.java
@@ -0,0 +1,22 @@
+package com.example.javacalismalari;
+
+import java.util.Scanner;
+
+public class javaDenemeleri {
+
+    public static void main(String[] args) {
+       System.out.println("Lütfen Noturnuzu giriniz : ");
+       Scanner s = new Scanner(System.in);
+       int i = s.nextInt();
+
+        if(i >= 80){
+           System.out.println("Notunuz : "+"AA");
+        }else(i >= 70){
+            
+        }
+
+    }
+}
+
+
+
